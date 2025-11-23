7bf7d6320660997fe8e54f294a51d91d234a2bd7
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-11-23 15:27:45 +0000

    .

--- backend/blog_generator/views.py
+++ backend/blog_generator/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
 def index(request):
     return render(request, 'index.html')
+
+def user_login(request):
+    pass
+
+def user_logout(request):
+    pass
+
+def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        repeatPassword = request.POST['repeatPassword']
+        if password == repeatPassword:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except:
+                error_message = 'Something went wrong, please try again later...'
+                return render(request, 'signup.html', {'error_message': error_message})
+        else:
+            error_message = 'Passwords do not match...'
+            return render(request, 'signup.html', {'error_message': error_message})
+    return render(request, 'signup.html')
