bd9d3308be8cfb185e3bd1b16cd1c7315f61e7ac
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-11-24 15:24:10 +0000

    .

--- backend/ai_blog_app/settings.py
+++ backend/ai_blog_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,3 +140,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ###
 LOGIN_URL = 'login'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- backend/blog_generator/views.py
+++ backend/blog_generator/views.py
@@ -1,24 +1,62 @@
 import json
+import os
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from pytube import YouTube
+import assemblyai as aai
+
 
 @login_required
 def index(request):
     return render(request, 'index.html')
 
+def yt_title(link):
+    yt = YouTube(link)
+    return yt.title
+
+def download_audio(link):
+    yt = YouTube(link)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path=settings.MEDIA_ROOT)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    return new_file
+
+def get_transcription(link):
+    audio_file = download_audio(link)
+    aai.settings.api_key = "ef647f668cc54efba526b8efe70a0486"
+    transcriber = aai.transcriber()
+    transcript = transcriber.transcribe(audio_file)
+    return transcript.text
+
+
 @csrf_exempt
 def generate_blog(request):
     if request.method == 'POST':
         try:
-            
+            data = json.loads(request.body) # why this and not request.POST
+            yt_link = data['link']
+            return JsonResponse({'content': yt_link})
         except (KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invald data sent'}, status=400)
+        # get yt title
+        title = yt_title(yt_link)
 
+        # get transcript
+        # use open ai to generate the nlob
+        # save blog article to database
+        # return blog article as a response
     else:
         return JsonResponse({'error': 'Invalid method'}, status=405)
+    
+
+    
 
 def user_login(request):
     if request.method == 'POST':
